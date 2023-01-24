3b665631fd22f934d26dc59826db2f1aa388dacf
Ekaterina <katya_usova@bk.ru>
2023-01-24 22:42:55 +0300

    HomeWork23

---
+++ HomeWork23/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Ekaterina
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            System.Console.Write("N = ");
+            int N = System.Convert.ToInt32(System.Console.ReadLine());
+            Cube(N);
+            System.Console.ReadKey(true);
+        }
+
+        static bool Cube(int N)
+        {
+            if(N < 1)
+            {
+                System.Console.Write("Число меньше 1");
+                return false;
+            }
+            for (int i = 1; i <= N; i++)
+            {
+                System.Console.Write("{0} ", System.Math.Pow(i, 3));
+            }
+            return true;
+        }
+    }
+}
